02edf5734cfc185420a25a9bf93f8a70c687b101
JCaasi74147 <caasi.jordan@ufl.edu>
2023-09-25 11:47:11 -0500

    added getters/setters/operator overloads

--- student.cpp
+++ student.cpp
@@ -6,6 +6,7 @@ using namespace std;
 class Student
 {
 public:
+    // Constructors and destructor
     Student() : name(""), ID(0), left(nullptr), right(nullptr) {};
     Student(string name, unsigned int id) : name(name), ID(id), left(nullptr), right(nullptr) {};
     Student(string name, unsigned int id, Student *left, Student *right) : name(name), ID(id), left(left), right(right) {};
@@ -14,6 +15,22 @@ public:
         left = nullptr;
         right = nullptr;
     }
+    // Getters and setters
+    string GetName() const { return name; }
+    unsigned int GetID() const { return ID; }
+    Student* GetLeft() const { return left; }
+    Student* GetRight() const { return right; }
+    void SetName(const string &n)  { name = n; }
+    void SetID(unsigned int id) { ID = id; }
+    Student& SetLeft(Student* left) { this->left = left; return *this; }    // return *this so we can method chain children pointers
+    Student& SetRight(Student* right) {this->right = right; return *this; }
+
+    // Operator overloading for comparisons
+    bool operator==(const Student &other) const { return ID == other.ID; }
+    bool operator!=(const Student &other) const { return ID != other.ID; }
+    bool operator<(const Student &other) const { return ID < other.ID; }
+    bool operator>(const Student &other) const { return ID > other.ID; }
+
 private:
     string name;
     unsigned int ID;
